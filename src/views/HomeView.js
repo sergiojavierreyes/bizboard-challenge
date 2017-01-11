927aa596000eb46869b472b3cdd57e06b351effb
Sergio Reyes <sergio.javier.reyes@me.com>
2017-01-12 00:01:22 +0100

    Figured out how to add components

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,12 +2,25 @@ import Surface              from 'famous/core/Surface.js';
 import {View}               from 'arva-js/core/View.js';
 import {layout, event}      from 'arva-js/layout/Decorators.js';
 
+
 export class HomeView extends View {
+
     @layout.size(~100, ~25)
     @layout.stick.center()
-    message = new Surface({content: `Hello ${this.options.welcomeName}`});
+    message = new Surface({
+        content:  
+        `<div> 
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+            <input type="text" placeholder="Message"/>
+            <br/>
+            <button>Submit Message</button>
+        </div>`
+    });
 
-    constructor(options = {}){
-        super(options);
-    }
 }
+
+  
