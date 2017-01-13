ca3e448f576243677e5b31647b044b3346d3db48
Sergio Reyes <sergio.javier.reyes@me.com>
2017-01-13 02:17:48 +0100

    Trying to get a functional button

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -5,15 +5,19 @@ import {ChatMessages}      from '../models/ChatContent.js';
 
 export class HomeController extends Controller {
 
-    Index(){
-        this.posts = new ChatMessages();
+	Index(){
+		this.posts = new ChatMessages();
 
-        if(!this.homeView) {
-            this.homeView = new HomeView();
-            this.homeView.singleInput.on('message', (input) => {
-            this.posts.add({text: input});
-            });
-        }
-        return this.homeView;
-    }
+		if(!this.homeView) {
+			this.homeView = new HomeView();
+			this.homeView.singleInput.on('post', (input) => {
+				this.posts.add({text: input});
+			});
+
+			// this.homeView.singleButton.on('click',  this.focus.bind(this));
+			// window.addEventListener('click', (event) =>{
+			// }.bind(this));
+		}
+		return this.homeView;
+	}
 }
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,109 +1,74 @@
 import Surface              	from 'famous/core/Surface.js';
 import InputSurface             from 'famous/surfaces/InputSurface.js';
 import {View}               	from 'arva-js/core/View.js';
-import {layout, event}      	from 'arva-js/layout/Decorators.js';
+import {layout, event, flow}   	from 'arva-js/layout/Decorators.js';
 import {ChatMessages}           from '../models/ChatContent.js';
 import {DataBoundScrollView}    from 'arva-js/components/DataBoundScrollView';
 
-import {SingleLineTextInput}    from 'arva-js/components/inputs/SingleLineTextInput.js';
+import Easing                   from 'famous/transitions/Easing.js';
+
 
 export class HomeView extends View {
 
-	   constructor(){
-        super();
+	constructor(){
+		super();
 
-        /* Create the renderable components we want to show in this view. */
-        this.renderables.chatView = new DataBoundScrollView({
-            itemTemplate: (post) => new AllMessages(post),
-            dataStore: new ChatMessages()
-        });
+		/* Create the renderable components we want to show in this view. */
+		/* Listing of all the messages with DataBoundScrollView. */
+		this.renderables.chatView = new DataBoundScrollView({
+			itemTemplate: (post) => new AllMessages(post),
+			dataStore: new ChatMessages()
+		});
 
-        this.renderables.input      = new InputSurface({placeholder: 'say Something'});
-		this.singleInput 	        = this.renderables.input;
+		/* Creating an input field and button with InputSurface */
+		this.renderables.input    = new InputSurface({placeholder: 'say Something'});
+		this.singleInput 	      = this.renderables.input;
 
-		this.renderables.button 	= new InputSurface({type: 'button',  value: 'Submit'});
-		this.singleButton 			= this.renderables.button;
+		// this.renderables.button   = new InputSurface({type: 'button',  value: 'Submit'});
+		// this.singleButton 		  = this.renderables.button;
 
-        /* Tell famous-flex how to render our components in Famous. */
-        this.layouts.push((context) => {
+		
+		/* Tell famous-flex how to render our components in Famous. */
+		this.layouts.push((context) => {
 
-            context.set('chatView', {
-                size: [undefined, context.size[1] * 0.9],
-                origin: [0.5, 0],
-                align: [0.5, 0],
-                translate: [0, 0, 10]
-            });
+			context.set('chatView', {
+				size: [undefined, context.size[1] * 0.9],
+				origin: [0.5, 0],
+				align: [0.5, 0],
+				translate: [0, 0, 10]
+			});
 
-            context.set('input', {
-                size: [undefined, 64],
-                origin: [0.5, 1],
-                align: [0.5, 1],
-                translate: [0, 0, 30]
-            });
-        });
-    }
+			context.set('input', {
+				size: [undefined, 64],
+				origin: [0.5, 1],
+				align: [0.5, 1],
+				translate: [0, 0, 30]
+			});
+		});
+	}
 }
 
 export class AllMessages extends View {
-    constructor(message){
-        super();
+	constructor(message){
+		super();
 
-        this.renderables.box = new Surface({
-            properties: {
-                backgroundColor: 'rgb(255, 255, 255)',
-                borderRadius: '4px',
-                boxShadow: '0px 2px 4px 0px rgba(50, 50, 50, 0.08)'
-            }
-        });
+		this.renderables.content = new Surface({
+			content: message.text
+		});
 
-        this.renderables.content = new Surface({
-            content: message.text,
-            properties: {
-                color: 'rgba(0, 0, 0, 0.8)'
-            }
-        });
+		this.layouts.push((context) => {
 
-        this.layouts.push((context) => {
-            context.set('box', {
-                size: [undefined, undefined]
-            });
+			context.set('content', {
+				size: [undefined, 20],
+				origin: [0, 0],
+				align: [0.1, 0.5],
+				translate: [0, 0, 10]
+			});
+		});
+	}
 
-            context.set('from', {
-                size: [undefined, 20],
-                origin: [0, 1],
-                align: [0.1, 0.45],
-                translate: [0, 0, 10]
-            });
-
-            context.set('content', {
-                size: [undefined, 20],
-                origin: [0, 0],
-                align: [0.1, 0.5],
-                translate: [0, 0, 10]
-            });
-        });
-    }
-
-    getSize(){
-        return [undefined, 64];
-    }
+	getSize(){}
 }
-
-// 	constructor(message){
-// 		super();
-
-// 		/* Create the renderable components we want to show in this view. */
-//         this.renderables.content = new Surface({
-//             content: message.content,
-//             properties: {
-//                 color: 'rgba(0, 0, 0, 0.8)'
-//             }
-//         });
-
-// 		 this.renderables.messageBox = new DataBoundScrollView({
-//             itemTemplate: (message) => new HomeView(message),
-//             dataStore: new ChatMessages()
-//         });
 
 // 		this.renderables.input      = new InputSurface({placeholder: 'say Something'});
 // 		this.singleInput 	        = this.renderables.input;
@@ -111,31 +76,3 @@ export class AllMessages extends View {
 // 		this.renderables.button 	= new InputSurface({type: 'button',  value: 'Submit'});
 // 		this.singleButton 			= this.renderables.button;
 
-// 		/* Tell famous-flex how to render our components in Famous. */
-// 		this.layouts.push((context) => {
-// 			context.set('messageBox', {
-// 				size: [undefined, undefined],
-// 				margins: [16, 16, 16, 16],
-//                 spacing: 8
-// 			});
-
-// 			context.set('input', {
-// 				size: [50, 50],
-// 				align: [0.7,0.8]
-// 			});
-
-// 			context.set('button', {
-// 				size: [true, true],
-// 				align: [0.5,0.6]
-// 			});
-
-//             context.set('content', {
-//                 size: [undefined, 20],
-//                 origin: [0, 0],
-//                 align: [0.1, 0.5],
-//                 translate: [0, 0, 10]
-//             });
-
-// 		});
-// 	}
-// }
