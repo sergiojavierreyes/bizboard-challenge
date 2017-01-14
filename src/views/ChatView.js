3d2023f1fbb07ab8c884976df81e4198c6be2498
Sergio Reyes <sergio.javier.reyes@me.com>
2017-01-14 18:41:44 +0100

    Random commit because Random

--- src/views/ChatView.js
+++
@@ -1,9 +0,0 @@
-import Surface              from 'famous/core/Surface.js';
-import {View}               from 'arva-js/core/View.js';
-import {layout, event}      from 'arva-js/layout/Decorators.js';
-
-
-export class ChatApp extends View {
-
-
-}
